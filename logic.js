571a620caca250087a675021bf397092296e9457
fabbeCode <fabianlindhe91@gmail.com>
2023-09-20 19:52:00 +0200

    Added new interactive function in javascript

---
+++ logic.js
@@ -0,0 +1,9 @@
+function favorite_pizza() { 
+    
+    var pizza = prompt("Hi there! tell us, which is your favorite pizza?")
+     
+    
+    alert(pizza + " Yes that really is a great choice!")
+
+} 
+
